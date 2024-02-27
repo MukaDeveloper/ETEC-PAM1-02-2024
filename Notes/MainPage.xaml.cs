a9c180bf5341a0ec1d3488c7e7e4e38b2704af2f
MukaDeveloper <muka.bertoni@gmail.com>
2024-02-26 21:44:50 -0300

    Initial commit

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Botão clicado {count} vez";
+            else
+                CounterBtn.Text = $"Botão clicado {count} vezes";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnHello(object sender, EventArgs e)
+        {
+            string nome = textInput.Text;
+            HelloLabel.Text = $"Hello, {nome}";
+            SemanticScreenReader.Announce(textInput.Text);
+        }
+    }
+
+}
